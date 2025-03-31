92abab07207234c7544723b65d88a97020b91aa2
ke.yi <ke.yi@sophgo.com>
2025-03-31 18:22:16 +0800

    e2fb7bbe674,fix watchdog question

--- distro/overlay/SOPHON/sophgo-fs/usr/sbin/bmrt_watchdog.sh
+++ distro/overlay/SOPHON/sophgo-fs/usr/sbin/bmrt_watchdog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo busybox echo V > /dev/watchdog0
 sudo busybox watchdog -t 3 -T 5 /dev/watchdog0
 sudo kill -9 $(ps -ef|grep watchdog0|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' )
 
